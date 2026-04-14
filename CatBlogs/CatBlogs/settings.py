b12ec2456e38b2f1514a0d84cb0c73c588fe868a
Russellopez <official.russellopez07@gmail.com>
2026-04-14 09:36:04 +0800

    modified:   CatBlogs/CatBlogs/settings.py

--- CatBlogs/CatBlogs/settings.py
+++ CatBlogs/CatBlogs/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'pandas',
     'crispy_forms',
     'crispy_bootstrap5',
-
 ]
 
 MIDDLEWARE = [
@@ -91,7 +90,7 @@ WSGI_APPLICATION = 'CatBlogs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
